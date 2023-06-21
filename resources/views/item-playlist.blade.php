13da6e9062efb9886a43894c37f2ade31fe84376
Juliana Hachmann <juliana.h28@aluno.ifsc.edu.br>
2023-06-21 16:27:17 -0300

    HTML item-playlist

---
+++ resources/views/item-playlist.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        .song {
+            border: 1px solid black;
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body>
+<div class="container playlist gx-5">
+    <div class="gx-5">   
+    
+        <span class="alert-dismissible fade show song col"> <span class="song-title">Nome da Música 1</span> - <span class="artist">Artista</span>
+        Ícone de excluir </span> 
+    
+        
+        </div>
+    
+</div>
+
+
+</body>
+</html>
