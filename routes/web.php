eb950e01ef177b80d11736ad20f9d400694ab9db
Juliana Hachmann <juliana.hachmann@gmail.com>
2023-06-29 11:05:21 -0300

    HelloWelcomePage

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello');
 });
 
 Route::post('count', function (Request $request) {
